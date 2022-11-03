bf6b83ee8041ee97323f30f169166d783c0af578
Junior Rosa <acsrosajunior@gmail.com>
2022-11-03 09:26:45 -0300

    teste

---
+++ script.js
@@ -0,0 +1,7 @@
+let num1 = Number(prompt('Digite um número'))
+let num2 = Number(prompt('Digite outro número'))
+
+let resultadoSoma = num1 + num2
+
+console.log(`a soma é ${resultadoSoma}`)
+alert(`a soma é ${resultadoSoma}`)
